e98f46025eef104e58b7150b66733e00054ec703
Tammy <samtammy54@gmail.com>
2024-06-06 12:08:48 +0100

    Add code to all files

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # Basketball-scoreboard
+
+A simple scoreboard with six buttons
+Contains two columns - Home and Guest
+The score increases by a certain number when you click on any of the buttons.
--- index.js
+++ index.js
@@ -1,1 +1,35 @@
+let homeCount = 0
+let guestCount = 0
+let homescoreEl = document.getElementById("homescore")
+let guestscoreEl = document.getElementById("guestscore")
 
+
+function homeIncrementOne() {
+    homeCount += 1
+    homescoreEl.textContent = homeCount
+}
+
+function homeIncrementTwo() {
+    homeCount += 2
+    homescoreEl.textContent = homeCount
+}
+
+function homeIncrementThree() {
+    homeCount += 3
+    homescoreEl.textContent = homeCount
+}
+
+function guestIncrementOne() {
+    guestCount += 1
+    guestscoreEl.textContent = guestCount
+}
+
+function guestIncrementTwo() {
+    guestCount += 2
+    guestscoreEl.textContent = guestCount
+}
+
+function guestIncrementThree() {
+    guestCount += 3
+    guestscoreEl.textContent = guestCount
+}
